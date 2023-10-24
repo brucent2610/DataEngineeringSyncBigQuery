aa645a32201a59e609a17ab1bdd5ffc6c8bb9895
Phong Nguyen <phong.nt.26101993@gmail.com>
2023-10-24 09:29:57 +0800

    add command startup

--- VM/startup.sh
+++ VM/startup.sh
@@ -5,4 +5,4 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update
 sudo apt install -y mongodb-org
-systemctl enable --now mongod
+sudo systemctl enable --now mongod
